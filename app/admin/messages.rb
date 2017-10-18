a6495f4dc85e8b9d0317f36d08c609ed348a1386
steven <steven@greatfeatservices.com>
2017-10-18 11:44:43 +0800

    fix messages

--- app/admin/messages.rb
+++ app/admin/messages.rb
@@ -2,4 +2,16 @@ ActiveAdmin.register Message do
   menu priority: 8, label: proc{ I18n.t("active_admin.messages") }
 
   actions :all, except: [:new, :create, :update, :edit]
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :company
+    column :phone
+    column :email
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
